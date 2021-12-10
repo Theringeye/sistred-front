8370d8c650d1f8045bac4e53face90103d34053a
Murilo Libaneo <mclibaneo@hotmail.com>
2021-12-10 16:17:24 -0300

    alter construct from cabos service

--- src/app/views/cabos/cabos.service.ts
+++ src/app/views/cabos/cabos.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Cabo } from "./cabo";
 
 @Injectable({
   providedIn: "root",
 })
 export class CabosService {
   URI:string = "http://localhost:8080/cabo";
-  constructor(private http: HttpClient) {}
-
+  cabos:Cabo[];
+  constructor(private http: HttpClient) {
+   //this.cabos = this.listar();
+    
+  }
+  listar(){
+    return this.http.get<{cabos: Cabo}[]>(this.URI);
+  }
   listarCabos() {
     this.http.get(this.URI).subscribe((data) => {
       console.log(data);
     });
   }
+ 
 }
