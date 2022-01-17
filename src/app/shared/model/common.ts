6dfe5eab0e42bf8fe04f9ccba2e3e190d0ccb85f
mclibaneo <mclibaneo@hotmail.com>
2022-01-17 10:45:11 -0300

    add herança com common.ts

---
+++ src/app/shared/model/common.ts
@@ -0,0 +1,6 @@
+export class Common {
+    id: number;
+    modelo: string;
+	fabricante: string;
+	patrimonio: string;
+}
--- src/app/views/teclados/teclado.ts
+++ src/app/views/teclados/teclado.ts
@@ -1,10 +1,7 @@
+import { Common } from "src/app/shared/model/common";
 import { Historico } from "src/app/shared/model/historico";
 
-export class Teclado{
-    id: number;
-    modelo: string;
-    fabricante: string;
-    patrimonio: string;
+export class Teclado extends Common{
     padrao: string;
     listaMovimentacaoDTO?: [];
     listaHistoricoAtivoDTO?: Historico[];
